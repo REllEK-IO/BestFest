8d310e76debb9a6d5bc1c3ef36e4a881af188821
individualist-null <micahtk@protonmail.com>
2017-05-10 14:42:07 -0700

    Work done on user api

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -1,22 +1,70 @@
 var db = require("../models");
 
 module.exports = function (app) {
-    // app.get("/api/authors", function(req, res) {
+    //Returns json of the requested user
+    //To do add admin verification
+    app.get("/api/user/id/:id", function (req, res) {
+        db.User.findOne({
+            where: {
+                id: req.body.id
+            }
+        }).then((data) => {
+            res.json(data);
+        })
+    });
 
-    // });
+    //Returns json of the requested user
+    app.get("/api/user/name/:name", function (req, res) {
+        db.User.findOne({
+            where: {
+                id: req.body.name
+            }
+        }).then((data) => {
+            res.json(data);
+        })
+    });
 
-    // app.get("/api/authors/:id", function(req, res) {
-
-    // });
-
+    //Creates a user.
     app.post("/api/user", function (req, res) {
         db.User.create(req.body).then(function (data) {
             res.json(data);
         });
     });
 
-    // app.delete("/api/authors/:id", function(req, res) {
+    app.put("api/update/user/id", function(req, res){
 
-    // });
+    });
 
-};
+    //Deletes user by id. Returns json of err or success
+    app.delete("/api/del/user/:id", function (req, res) {
+        //Find out if current user is an admin
+        console.log(req.body);
+        db.User.findOne({
+            where: {
+                user_name: req.body.user
+            }
+        }).then((data => {
+            if (data.user_type === "admin" || Number(req.params.id) === Number(data.id)) {
+                if (data.user_type === "admin" && Number(data.id) === Number(req.params.id)) {
+                    res.json({
+                        err: "You cannot delete yourself"
+                    });
+                } else {
+                    db.User.destroy({
+                        where: {
+                            id: req.params.id
+                        }
+                    }).then((fin) => {
+                        res.json({
+                            success: ("User ID: " + fin + " Was deleted")
+                        });
+                    })
+                }
+            } else {
+                res.json({
+                    err: "Fuck You."
+                });
+            }
+        }));
+    });
+}

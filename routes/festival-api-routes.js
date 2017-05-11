6ad4434bb1fd332d4ed37c1881c537844ac1400d
Micah <micah.t.keller@gmail.com>
2017-05-10 17:29:15 -0700

    Reduced fesitval api routes

--- routes/festival-api-routes.js
+++ routes/festival-api-routes.js
@@ -22,62 +22,6 @@ module.exports = function(app) {
 
     });
 
-
-    // POST route for saving a new post
-    app.post("/api/review", function(req, res) {
-        var newReview = {};
-        var user = req.body.user;
-
-        db.User.create({
-            user_name: "Ass"
-        }).then(() => {
-            db.User.create({
-                user_name: "ewok"
-            }).then(() => {
-                db.User.create({
-                    user_name: "Tastic"
-                });
-            }).then(() => {
-                for (key in req.body) {
-                    switch (key) {
-                        case "title":
-                            newReview["title"] = req.body[key];
-                            break;
-                        case "festival":
-                            newReview["festival"] = req.body[key];
-                            break;
-                        case "overall":
-                            newReview["overall"] = Number(req.body[key]);
-                            break;
-                        case "security":
-                            newReview["security"] = Number(req.body[key]);
-                            break;
-                        case "text_body":
-                            newReview["text_body"] = req.body[key];
-                            break;
-                        case "tags":
-                            newReview["tags"] = req.body[key];
-                            break;
-                    }
-                }
-
-                db.User.findOne({
-                    where: {
-                        user_name: user
-                    }
-                }).then(function(usr) {
-                    newReview["user_id"] = usr.id;
-                    console.log(usr.id);
-                    db.Review.create(newReview, {
-                        include: [db.User]
-                    }).then((resp) => {
-                        res.json(resp);
-                    })
-                })
-            });
-        });
-    });
-
     // DELETE route for deleting festival
     app.delete("/api/festival/:id", function(req, res) {
 

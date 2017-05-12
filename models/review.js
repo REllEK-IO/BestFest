02c696665a7de340cda65de8554da474af75c3fe
Andrew Gardner <andrewgardner@Andrews-MacBook-Air-2.local>
2017-05-11 21:21:58 -0700

    ajax form object complete

--- models/review.js
+++ models/review.js
@@ -24,6 +24,13 @@ module.exports = function(sequelize, DataTypes) {
                 len: [1]
             }
         },
+        year: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
         //Overall score of festival
         overall: {
             type: DataTypes.INTEGER,
--- public/assets/js/form.js
+++ public/assets/js/form.js
@@ -1,38 +1,47 @@
-// jquery
-// don't use node, this is a client file
-// val.trim
-
-// var host = window.location.host + ""/api/review""
-
-// $( "#submit" ).click(function() {
-//     event.preventDefault();
-//     var festival = $("#select-name").val().trim();
-//     console.log(festival);
-
-//     var year = $("#year-select").val().trim();
-//     console.log(year);
-
-//     var cost = $(".cost").val().trim();
-//     console.log(cost);
-
-//     var security = $(".security").val().trim();
-//     console.log(security);
-
-//     var sound = $(".sound").val().trim();
-//     console.log(sound);
-
-//     var rating = $("#3-star").val().trim();
-//     console.log(rating);
-// });
-
 $(document).ready(function(){
     $("#submit").click(function(){
-        $.post(window.location.host + "/api/review",
+        $.post("/api/review",
         {
-          festival: $("#select-name").val().trim()
+            festival : $("#select-name").val().trim(),
+            year : $("#year-select").val().trim(),
+            overall : $("input[type=radio][name=rating]:checked").val().trim(),
+            cost : $("input[type=radio][name=cost]:checked").val().trim(),
+            security : $("input[type=radio][name=security]:checked").val().trim(),
+            sound: $("input[type=radio][name=sound]:checked").val().trim(),
+            title : $("#title").val().trim(),
+            text_body : $("#text-review").val().trim()
         },
-        function(data,status){
-            console.log("Data: " + data + "\nStatus: " + status);
+        function(data, status){
+            console.log("Data: " + data.title + "\nStatus: " + status);
         });
     });
 });
+
+
+// $("#submit").click(function(){
+//     var reviewFestival = $("#select-name").val().trim();
+//     console.log(reviewFestival);
+
+//     var reviewYear = $("#year-select").val().trim();
+//     console.log(reviewYear);
+
+//     var cost = $("input[type=radio][name=cost]:checked").val().trim();
+//     console.log(cost);
+
+//     var security = $("input[type=radio][name=security]:checked").val().trim();
+//     console.log(security);
+
+//     var sound = $("input[type=radio][name=sound]:checked").val().trim();
+//     console.log(sound);
+
+//     var rating = $("input[type=radio][name=rating]:checked").val().trim();
+//     console.log(rating);
+
+//     var reviewTitle = $("#title").val().trim();
+//     console.log(reviewTitle);
+
+//     var reviewBody = $("#text-review").val().trim();
+//     console.log(reviewBody);
+// });
+
+
